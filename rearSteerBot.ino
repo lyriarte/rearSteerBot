2c0fdf193bc75179e26bb6d0ecdd581ed482d062
Luc Yriarte <lyriarte@gmail.com>
2015-06-13 19:54:33 +0200

    left / right ulstrasonic sensors

--- rearSteerBot.ino
+++ rearSteerBot.ino
@@ -5,14 +5,19 @@
 #define INLEFT 10
 #define INRIGHT 11
 #define INRUN 12
+#define INLEFTECHO 2
+#define LEFTTRIGGER 3
+#define INRIGHTECHO 4
+#define RIGHTTRIGGER 5
 
+#define POLL 50
+
+#define STOP 0
 #define STRAIGHT 90
 #define LEFT 120
 #define RIGHT 60
 
-
 Servo steerServo;
-int pollms;
 
 void setup() {
 	pinMode(STEERSERVO, OUTPUT);
@@ -20,10 +25,16 @@ void setup() {
 	pinMode(INRIGHT, INPUT);
 	pinMode(ENGINERELAY, OUTPUT);
 	pinMode(INRUN, INPUT);
+	pinMode(INLEFTECHO, INPUT);
+	pinMode(LEFTTRIGGER, OUTPUT);
+	pinMode(INRIGHTECHO, INPUT);
+	pinMode(RIGHTTRIGGER, OUTPUT);
 	steerServo.attach(STEERSERVO);
-	pollms=50;
+	Serial.begin(9600);
 	steerServo.write(STRAIGHT);
 	digitalWrite(ENGINERELAY, LOW);
+	digitalWrite(LEFTTRIGGER, LOW);
+	digitalWrite(RIGHTTRIGGER, LOW);
 }
 
 int getSteer() {
@@ -36,6 +47,18 @@ int getSteer() {
 
 
 void loop() {
+	unsigned long echoDuration;
+	digitalWrite(LEFTTRIGGER, HIGH);
+	digitalWrite(LEFTTRIGGER, LOW);
+	Serial.print("Reading... ");
+	echoDuration = pulseIn(INLEFTECHO, HIGH, 100000);
+	Serial.print(echoDuration / 60);
+	digitalWrite(RIGHTTRIGGER, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(RIGHTTRIGGER, LOW);
+	Serial.print("... ");
+	echoDuration = pulseIn(INRIGHTECHO, HIGH, 100000);
+	Serial.println(echoDuration / 60);
 	steerServo.write(getSteer());
 	if (digitalRead(INRUN) == HIGH) {
 		digitalWrite(ENGINERELAY, HIGH);
@@ -43,6 +66,6 @@ void loop() {
   	else {
 		digitalWrite(ENGINERELAY, LOW);
 	}
-	delay(pollms);
+	delay(POLL);
 }
 
